4651fc6286005bb010937a09489c88476dda241e
Shama Hegde <shamap.hegde@gmail.com>
2025-11-22 09:50:20 +0530

    updated to vite

--- app/(tabs)/index.tsx
+++
@@ -1,9 +0,0 @@
-import { View, Text } from 'react-native';
-
-export default function HomeScreen() {
-  return (
-    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
-      <Text>Hello from Expo SDK 54 🚀</Text>
-    </View>
-  );
-}
